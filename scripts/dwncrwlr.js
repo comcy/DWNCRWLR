35ca59df23013ae424a8d7004b00a53939455892
com_cy <christian.silfang@googlemail.com>
2018-11-12 21:49:30 +0100

    started foreach to iterate to evry file matching glob pattern, read file info and concatenate them with dist path

--- scripts/dwncrwlr.js
+++ scripts/dwncrwlr.js
@@ -3,9 +3,16 @@
  */
 
 const fs = require('fs-extra');
+const glob = require('glob');
+const path = require('path');
+const ejs = require('ejs');
+const marked = require('marked');
+const showdown = require('showdown');
+const frontMatter = require('front-matter');
+
 const config = require('../dwncrwlr.config.json');
 const styles = require('./console-style')
-const glob = require('glob');
+
 
 // Variabel declaration defined in dwncrwlr.config.json
 const srcPath = config.build.srcPath;
@@ -14,19 +21,19 @@ const distPath = config.build.distPath;
 
 // Start of dwncrwlr script
 console.log(styles.textFgRed, styles.asciiLogo);
-console.log('\n', styles.textFgGreen);
+console.log('\n', styles.textFgMagenta);
 console.log('//-----------------------------------------------------');
 console.log('\t Starting static site generation');
 console.log('//-----------------------------------------------------');
 console.log('\n', styles.styleReset);
 
-// Empty dist folder
+// 1. Empty dist folder
 fs.emptyDirSync(distPath);
 
-// Copy required assets to dist folder
+// 2. Copy required assets to dist folder
 fs.copy(`${srcPath}/assets`, `${distPath}/assets`);
 
-// Read in all files by specified pattern
+// 3. Read in all files by specified pattern
 console.log('Supported file extensions:');
 console.log(styles.textFgGreen, `${config.build.supportedContentExtensionsPattern}`);
 
@@ -35,9 +42,25 @@ const files = glob.sync(`**/*.@(${config.build.supportedContentExtensionsPattern
 console.log('\n', styles.styleReset);
 console.log('Detected files:\n', files);
 
+// 4. Iterate through all files
+files.forEach((file) => {
+
+    // Read file information of every single file ...
+    const fileInfo = path.parse(file); // get complete file info
+    console.log(fileInfo); // TODO: remove later
+    
+    // ... and concatenate them with the dist folder path
+    const fileCopyPath = path.join(distPath, fileInfo.dir)
+    console.log(fileCopyPath); // TODO: remove later
+
+    
 
 
-console.log('\n', styles.textFgGreen);
+
+
+});
+
+console.log('\n', styles.textFgMagenta);
 console.log('//-----------------------------------------------------');
 console.log('\t Finished static site generation');
 console.log('//-----------------------------------------------------');
