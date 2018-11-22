2d755e6a8f1911a17300107ed2e945176b82f49a
com_cy <christian.silfang@googlemail.com>
2018-11-22 20:18:32 +0100

    removed generated *.js files

--- typescript/helpers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var console_style_1 = require("./console-style");
-exports.consoleStyle = console_style_1.consoleStyle;
-var read_file_content_function_1 = require("./read-file-content.function");
-exports.readFileContents = read_file_content_function_1.readFileContents;
-var navigation_collection_1 = require("./navigation-collection");
-exports.NavigationCollection = navigation_collection_1.NavigationCollection;
-//# sourceMappingURL=index.js.map
--- typescript/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var navigation_item_model_1 = require("./navigation-item.model");
-exports.NavigationItem = navigation_item_model_1.NavigationItem;
-//# sourceMappingURL=index.js.map
