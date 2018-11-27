aadae85c978cdb80e735d4d9e9e3989c342535f6
Christian Silfang <christian.silfang@googlemail.com>
2018-11-27 07:38:02 +0100

    Delete index.js
    remove *.js files from src/

--- src/helpers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var console_style_1 = require("./console-style");
-exports.consoleStyle = console_style_1.consoleStyle;
-var read_file_content_function_1 = require("./read-file-content.function");
-exports.readFileContents = read_file_content_function_1.readFileContents;
-var arraylist_multimap_1 = require("./arraylist-multimap");
-exports.ArrayListMultimap = arraylist_multimap_1.ArrayListMultimap;
-//# sourceMappingURL=index.js.map
