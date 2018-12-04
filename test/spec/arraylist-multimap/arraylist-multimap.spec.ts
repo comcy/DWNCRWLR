1d48acd74ec70b4facebb906d49483196a59ebff
com_cy <christian.silfang@googlemail.com>
2018-12-04 21:39:07 +0100

    added initial jasmin test and made tsc setup

---
+++ test/spec/arraylist-multimap/arraylist-multimap.spec.ts
@@ -0,0 +1,15 @@
+import 'jasmine';
+import { ArrayListMultimap } from "src/helpers/index";
+
+const arrayList = new ArrayListMultimap<string, string>();
+
+arrayList.put('one', '1');
+arrayList.put('two', '2');
+arrayList.put('three', '3');
+
+
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(arrayList.containsKey('one')).toBe(true);
+    });
+});
