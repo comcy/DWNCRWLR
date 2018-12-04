eebf52b159ef7d0eb5ea9a4f00ece2583af9414a
com_cy <christian.silfang@googlemail.com>
2018-12-04 22:16:43 +0100

    configured karma

--- test/spec/arraylist-multimap/arraylist-multimap.spec.ts
+++ test/spec/arraylist-multimap/arraylist-multimap.spec.ts
@@ -1,12 +1,11 @@
 import 'jasmine';
-import { ArrayListMultimap } from "src/helpers/index";
+import { ArrayListMultimap } from "../../../src/helpers/arraylist-multimap";
 
 const arrayList = new ArrayListMultimap<string, string>();
 
 arrayList.put('one', '1');
 arrayList.put('two', '2');
 arrayList.put('three', '3');
-
 
 describe("A suite", function () {
     it("contains spec with an expectation", function () {
