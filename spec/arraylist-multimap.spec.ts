8156817eea63324991d7935e11a25b0be038c6d9
com_cy <christian.silfang@googlemail.com>
2018-12-06 22:22:38 +0100

    extended some test cases

--- spec/arraylist-multimap.spec.ts
+++ spec/arraylist-multimap.spec.ts
@@ -8,8 +8,33 @@ arrayList.put('one', '1');
 arrayList.put('two', '2');
 arrayList.put('three', '3');
 
+// CONTAINS KEY
 describe("ArrayList Multimap", function () {
     it("contains a key named 'one'", function () {
         expect(arrayList.containsKey('one')).toBe(true);
+    });
+});
+
+
+// CONTAINS VALUE
+describe("ArrayList Multimap", function () {
+    it("contains a value named '1'", function () {
+        expect(arrayList.containsValue('1')).toBe(true);
+    });
+});
+
+
+// CONTAINS ENTRY
+describe("ArrayList Multimap", function () {
+    it("contains a entry with key 'one' and value '1'", function () {
+        expect(arrayList.containsEntry('one', '1')).toBe(true);
+    });
+});
+
+
+// CONTAINS ALL ENTRIES
+describe("ArrayList Multimap", function () {
+    it("contains a entry with a key and a value", function () {
+        expect(arrayList.containsEntry('one', '1')).toBe(true);
     });
 });
